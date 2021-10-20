58afb02dedcad492560e98fc6944dc2b896bd8d7
Daniel Wentsch <hello@wentsch.me>
2021-10-20 09:27:45 +0200

    Improve comment

--- loginscript.sh
+++ loginscript.sh
@@ -1,2 +1,6 @@
-# Remap Capslock to Backspace, run this on ecery login
+# Remap Capslock to Backspace
+# Run this on every login, from within the directory of this script run:
+# 1. `chmod +x loginscript.sh`
+# 2. `sudo defaults write com.apple.loginwindow LoginHook `pwd`/loginscript.sh`
+
 hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000002A}]}'
