34915fa91d9ab2a61addf12eb5e581b2fc12f9c8
Daniel Wentsch <hello@wentsch.me>
2021-10-20 09:26:32 +0200

    Remap capslock to backspace on login

---
+++ loginscript.sh
@@ -0,0 +1,2 @@
+# Remap Capslock to Backspace, run this on ecery login
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000002A}]}'
