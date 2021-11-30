25147e92902429ffbff6232829239757bce968c6
Daniel Wentsch <hello@wentsch.me>
2021-11-30 10:42:09 +0100

    Add homebrew bin to the right spot

--- path.zsh
+++ path.zsh
@@ -2,4 +2,4 @@ export PATH="$PATH:$HOME/.composer/vendor/bin"
 export PATH=$PATH:/usr/local/sbin/
 export PATH=$PATH:/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
 export PATH=/usr/local/bin:$PATH
-
+export PATH="/opt/homebrew/bin:$PATH"
